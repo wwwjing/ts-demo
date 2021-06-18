488b4f52285b3657770fabf4191747e8b6db256e
TAL <tal@TALdeMacBook-Pro.local>
2021-06-18 10:55:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function q() {
+    var Color;
+    (function (Color) {
+        Color[Color["Red"] = 1] = "Red";
+        Color[Color["Green"] = 2] = "Green";
+        Color[Color["Blue"] = 3] = "Blue";
+    })(Color || (Color = {}));
+    var c = Color.Green;
+    console.log(c);
+}
+q();
---
+++ index.ts
@@ -0,0 +1,7 @@
+function q () {
+    enum Color {Red = 1, Green, Blue}
+    let c: Color = Color.Green;
+    console.log(c);
+    
+}
+q();
